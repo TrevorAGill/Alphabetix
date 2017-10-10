493eca637acaf8f4b91f80e0ade3491ca997e1b8
wedaft <trevor.a.gill@gmail.com>
2017-10-10 15:44:56 -0700

    grid displays all letters and implemented custom font

--- app/src/main/java/com/epicodus/alphabetix/MainActivity.java
+++ app/src/main/java/com/epicodus/alphabetix/MainActivity.java
@@ -1,5 +1,6 @@
 package com.epicodus.alphabetix;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.GridView;
@@ -8,9 +9,13 @@ public class MainActivity extends AppCompatActivity {
     GridView gridView;
     String[] letters = new String[] {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Typeface typeface = Typeface.createFromAsset(getAssets(), "fonts/atlantic-bentley.ttf");
+        gridView = (GridView) findViewById(R.id.baseGridView);
+        gridView.setAdapter(new AlphabetAdapter(this, letters, typeface));
     }
 }

bb1a41efdbfd43b64000343d208a2897c05e0830
wedaft <trevor.a.gill@gmail.com>
2017-10-10 16:37:10 -0700

    grid displays all letters and implemented custom font

---
+++ app/src/main/java/com/epicodus/alphabetix/AlphabetAdapter.java
@@ -0,0 +1,62 @@
+package com.epicodus.alphabetix;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class AlphabetAdapter extends BaseAdapter {
+    private Context mContext;
+    private String[] mLetters;
+    private Typeface mTypeface;
+
+    public AlphabetAdapter(Context mContext, String[] mLetters, Typeface mTypeface) {
+        this.mContext = mContext;
+        this.mLetters = mLetters;
+        this.mTypeface = mTypeface;
+    }
+
+    @Override
+    public int getCount() {
+        return mLetters.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        String letter = mLetters[i];
+        return letter;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View gridView;
+
+        if (convertView == null) {
+            // get layout from xml file
+            gridView = inflater.inflate(R.layout.alphabet_grid_item, null);
+
+
+            // pull views
+            TextView letterView = (TextView) gridView
+                    .findViewById(R.id.grid_item_letter);
+
+            // set values into views
+            letterView.setText(mLetters[position]);  // using dummy data for now
+            letterView.setTypeface(mTypeface);
+        } else {
+            gridView = (View) convertView;
+        }
+        return gridView;
+    }
+}
